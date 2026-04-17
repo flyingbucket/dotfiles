f67ac83d32f556d54fbacfc45b03413fa09840d6
flyingbucket <2311067@mail.nankai.edu.cn>
2026-04-17 16:14:31 +0800

    bash: export texlive path

--- bash/.bashrc.d/texlive.sh
+++ bash/.bashrc.d/texlive.sh
@@ -1,2 +1,4 @@
 # texlive
-export PATH=$PATH:/home/flyingbucket/apps/texlive/2025/bin/x86_64-linux
+export PATH="$HOME/apps/texlive/2025/bin/x86_64-linux:$PATH"
+export MANPATH="$HOME/apps/texlive/2025/texmf-dist/doc/man:$MANPATH"
+export INFOPATH="$HOME/apps/texlive/2025/texmf-dist/doc/info:$INFOPATH"
