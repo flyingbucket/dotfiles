5d4fe29e45a2386aeb04775055faee43f88097b5
flyingbucket <2311067@mail.nankai.edu.cn>
2026-04-24 12:39:10 +0800

    bash: update texlive

--- bash/.bashrc.d/texlive.sh
+++ bash/.bashrc.d/texlive.sh
@@ -1,4 +1,4 @@
 # texlive
-export PATH="$HOME/apps/texlive/2025/bin/x86_64-linux:$PATH"
-export MANPATH="$HOME/apps/texlive/2025/texmf-dist/doc/man:$MANPATH"
-export INFOPATH="$HOME/apps/texlive/2025/texmf-dist/doc/info:$INFOPATH"
+export PATH="$HOME/apps/texlive/current/bin/x86_64-linux:$PATH"
+export MANPATH="$HOME/apps/texlive/current/texmf-dist/doc/man:$MANPATH"
+export INFOPATH="$HOME/apps/texlive/current/texmf-dist/doc/info:$INFOPATH"
