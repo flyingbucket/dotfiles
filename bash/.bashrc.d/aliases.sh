9973da47b288861dc2f93c2a294dd568ebd4cfdc
flyingbucket <menglinli598@gmail.com>
2026-07-26 21:08:03 +0800

    bash(alias): eza for ls

--- bash/.bashrc.d/aliases.sh
+++ bash/.bashrc.d/aliases.sh
@@ -15,3 +15,11 @@ fi
 if [ -f "$HOME/CODE/scripts/rnvim" ]; then
   alias rnvim='$HOME/CODE/scripts/rnvim'
 fi
+
+if command -v eza &>/dev/null; then
+  alias ls='eza -al --color=always --group-directories-first --icons=always' # preferred listing
+  alias la='eza -a --color=always --group-directories-first --icons=always'  # all files and dirs
+  alias ll='eza -l --color=always --group-directories-first --icons=always'  # long format
+  alias lt='eza -aT --color=always --group-directories-first --icons=always' # tree listing
+  alias l.="eza -a | grep -e '^\.'"                                          # show only dotfiles
+fi
